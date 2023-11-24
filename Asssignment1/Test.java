68e40ca1db13298c5ca36dc34b308884aef6384e
Kostadinova19 <147843312+Kostadinova19@users.noreply.github.com>
2023-11-24 18:14:21 +0100

    Create Test.java

---
+++ Asssignment1/Test.java
@@ -0,0 +1,35 @@
+package exercise2;
+
+public class Test {
+	    private String s;
+
+	    public Test(String s) {
+	        this.s = s;
+	    }
+	    public Test() {
+	        this.s = "Programming languages";
+	    }
+
+	    public void method1() {
+	        System.out.println(s);
+	    }
+
+	    public void method2(int n) {
+	        System.out.println(n);
+	    }
+
+	    private void method3() {
+	        System.out.println("Private method is invoked");
+	    }
+
+	    public static void main(String[] args) {
+	        
+	        Test testDefault = new Test();
+	        testDefault.method1();
+	        testDefault.method2(42);
+
+	        Test testWithParameter = new Test("Hello, World!");
+	        testWithParameter.method1();
+	        testWithParameter.method2(99);
+	    }
+	}
